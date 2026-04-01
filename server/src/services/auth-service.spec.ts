95dc0ecf44522274754ab98660363c350ab11330
arvid-e <arvid.edvinsson@gmail.com>
2026-04-01 22:47:19 +0900

    Add tests cases to auth service

--- server/src/services/auth-service.spec.ts
+++ server/src/services/auth-service.spec.ts
@@ -1,23 +1,110 @@
+import { IAuthService } from "@app/interface/auth-service";
+import { ITokenService } from "@app/interface/token-service";
+import { IUserCredentials, IUserDocument } from "@app/interface/user";
+import { IUserRepository } from "@app/interface/user-repository";
+import bcrypt from 'bcryptjs';
+import { Types } from "mongoose";
 import { describe, it } from "vitest";
 import { AuthService } from "./auth-service";
-import { IUserRepository } from "@app/interface/user-repository";
-import { ITokenService } from "@app/interface/token-service";
 
-describe('', () => {
+const TEST_USERNAME = 'testuser';
+const TEST_PASSWORD = 'testpassword123';
+const ACCESS_TOKEN = 'access123';
+const REFRESH_TOKEN = 'refresh123';
 
-    const mockUserRepo = {
-        findById: vi.fn(),
-        findByUserName: vi.fn(),
-        create: vi.fn()
-    } as unknown as IUserRepository
+vi.mock('bcryptjs', () => ({
+    default: {
+        compare: vi.fn(),
+        hash: vi.fn(),
+    },
+}));
 
-    const mockTokenService = {
-        generateAccessPair: vi.fn(),
-    } as unknown as ITokenService
+describe('authService', () => {
+    let mockUserRepo: IUserRepository;
+    let mockTokenService: ITokenService;
+    let authService: IAuthService;
 
-    const authService = new AuthService(mockUserRepo, mockTokenService)
+    const createMockUser = () => ({
+        _id: new Types.ObjectId(),
+        username: TEST_USERNAME,
+        password: TEST_PASSWORD
+    } as IUserDocument);
 
-    it('', () => {
+    const createMockTokenResponse = () => ({
+        accessToken: ACCESS_TOKEN,
+        refreshToken: REFRESH_TOKEN
+    });
 
+    const credentials: IUserCredentials = {
+        username: TEST_USERNAME,
+        password: TEST_PASSWORD
+    }
+
+    beforeEach(() => {
+        mockUserRepo = {
+            findById: vi.fn(),
+            findByUsername: vi.fn(),
+            create: vi.fn()
+        } as unknown as IUserRepository;
+
+        mockTokenService = {
+            generateAccessPair: vi.fn(),
+        } as unknown as ITokenService;
+
+        authService = new AuthService(mockUserRepo, mockTokenService);
+    });
+
+    describe('login()', () => {
+        it('should return the user and token on successfull login', async () => {
+            const mockUser = createMockUser();
+            const mockTokenResponse = createMockTokenResponse();
+
+            vi.mocked(mockUserRepo.findByUsername).mockResolvedValue(mockUser)
+            vi.mocked(mockTokenService.generateAccessPair).mockResolvedValue(mockTokenResponse)
+            vi.mocked(bcrypt.compare).mockResolvedValue(true as never);
+
+            const result = await authService.login(credentials)
+
+            expect(result.tokens.accessToken).toBe(ACCESS_TOKEN);
+            expect(result.tokens.refreshToken).toBe(REFRESH_TOKEN);
+            expect(result.user.username).toBe(TEST_USERNAME)
+        })
+
+        it('should throw error on invalid credentials', async () => {
+            const mockUser = createMockUser();
+            const mockTokenResponse = createMockTokenResponse();
+
+            vi.mocked(mockUserRepo.findByUsername).mockResolvedValue(mockUser)
+            vi.mocked(mockTokenService.generateAccessPair).mockResolvedValue(mockTokenResponse)
+            vi.mocked(bcrypt.compare).mockResolvedValue(false as never);
+
+            await expect(authService.login(credentials))
+                .rejects
+                .toThrow('Invalid username or password.');
+        })
+
+        it('should throw error on when user not found', async () => {
+            vi.mocked(mockUserRepo.findByUsername).mockResolvedValue(null)
+
+            await expect(authService.login(credentials))
+                .rejects
+                .toThrow('Invalid username or password.');
+        })
+    })
+
+    describe('register()', () => {
+        it('should return the user and token on successfull register', async () => {
+            const mockUser = createMockUser();
+            const mockTokenResponse = createMockTokenResponse();
+
+            vi.mocked(mockUserRepo.create).mockResolvedValue(mockUser)
+            vi.mocked(mockTokenService.generateAccessPair).mockResolvedValue(mockTokenResponse)
+
+            const result = await authService.register(credentials)
+
+            expect(result.tokens.accessToken).toBe(ACCESS_TOKEN);
+            expect(result.tokens.refreshToken).toBe(REFRESH_TOKEN);
+            expect(result.user.username).toBe(TEST_USERNAME)
+        })
     })
 })
