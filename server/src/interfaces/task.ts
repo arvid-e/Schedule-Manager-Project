f3ccf0c59e1a589520a3c85bd842a51ec08f3bdb
arvid-e <arvid.edvinsson@gmail.com>
2026-05-18 23:52:43 +0900

    Edit task interfaces

--- server/src/interfaces/task.ts
+++ server/src/interfaces/task.ts
@@ -3,16 +3,17 @@ import { Document } from 'mongoose';
 export interface ITask {
   title: string;
   description?: string;
-  startDate?: Date;
-  endDate?: Date;
+  date: Date;
 }
 
 export interface IUpdateTask {
   _id: string;
   title?: string;
   description?: string;
-  startDate?: Date;
-  endDate?: Date;
+  date: Date;
 }
 
 export interface ITaskDocument extends ITask, Document {}
+
+
+
