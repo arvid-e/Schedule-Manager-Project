7c6a65a53387aaeaac65bd6ad995d41b93920f75
arvid-e <arvid.edvinsson@gmail.com>
2026-05-18 23:50:26 +0900

    Create full year of weeks

--- server/src/utils/date-utils.ts
+++ server/src/utils/date-utils.ts
@@ -1,6 +1,8 @@
+import { Week, WeekDay } from '../interfaces/task.js';
+
 /**
  * Gets the weekdays of the first week of the year that belongs to specified year.
- * 
+ *
  * @param date - Date that specified what year to use.
  * @returns - Array of dates that belongs to the first week of the year that are within the specified year.
  */
@@ -44,12 +46,12 @@ export function getFirstWeekOfTheYear(date = new Date()): Date[] {
 
 /**
  * Fills an incomplete first week of the year with the dates that belongs to the previous year.
- * 
+ *
  * @param incompleteWeek - Incomplete first week of the year.
  * @returns - The complete first week of the year.
  */
 export function fillMissingDaysInWeek(incompleteWeek: Date[]): Date[] {
-  if (incompleteWeek[0].getDay() === 1) {
+  if (incompleteWeek[0].getUTCDay() === 1) {
     const fullWeek = incompleteWeek;
     return fullWeek;
   }
@@ -63,4 +65,45 @@ export function fillMissingDaysInWeek(incompleteWeek: Date[]): Date[] {
   }
 
   return fullWeek;
+}
+
+/**
+ * Creates a full year of weeks with week numbers, dates and empty task arrays.
+ * 
+ * @returns - One year of week objects.
+ */
+export function createWeeks() {
+  const sparseFirstWeek = getFirstWeekOfTheYear();
+  const fullFirstWeek = fillMissingDaysInWeek(sparseFirstWeek);
+
+  const currentYear = fullFirstWeek[6].getUTCFullYear();
+  let currentDay = fullFirstWeek[0];
+
+  const fullYearOfWeeks = [];
+  let weekNumber = 0;
+
+  while (currentDay.getUTCFullYear() < currentYear + 1) {
+    let week: Week = {
+      weekNumber: 0,
+      weekDays: [],
+    };
+
+    for (let i = 0; i < 7; i++) {
+      currentDay.setUTCDate(currentDay.getUTCDate() + 1);
+
+      let weekDay: WeekDay = {
+        date: new Date(),
+        tasks: [],
+      }
+
+      week.weekDays.push(weekDay);
+    }
+
+    weekNumber += 1;
+    week.weekNumber = weekNumber;
+
+    fullYearOfWeeks.push(week);
+  }
+
+  return fullYearOfWeeks;
 }
