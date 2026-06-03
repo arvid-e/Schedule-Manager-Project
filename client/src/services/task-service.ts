b02ce3970974f670ff5f9b008c0b39d30ee152ff
arvid-e <arvid.edvinsson@gmail.com>
2026-06-03 17:16:00 +0900

    Create service and component for creating tasks on frontend

--- client/src/services/task-service.ts
+++ client/src/services/task-service.ts
@@ -1,6 +1,25 @@
+import type { Task } from '../interfaces/task';
+
 export const getWeek = async (weekNumber: number) => {
   const response = await fetch(`/api/v1/tasks/week/${weekNumber}`);
   const data = await response.json();
 
   return data.data;
+};
+
+export const createTask = async (task: Task) => {
+  const response = await fetch(`/api/v1/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: task.title,
+      description: task.description,
+      date: task.date,
+    }),
+  });
+  const data = await response.json();
+
+  return data;
 };
